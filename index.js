145b10aa4e84415a82a5708b8beb991f952edbc1
olivia.mo <olivia.mo@carsguide.com.au>
2018-04-08 13:28:21 +1000

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+// app to decide what to eat for us
+
+var places = ['McDonalds', 'KFC', 'Hungry Jacks', 'Cabramatta'];
+
+function randomiser() {
+  var decider = Math.floor(Math.random() * places.length);
+  console.log(decider);
+  return decider;
+};
+
+document.querySelector('.btn').addEventListener('click', () => {
+  var number = randomiser();
+  document.querySelector('.go-here').innerHTML = places[number];
+});
---
+++ readme.md
@@ -0,0 +1,3 @@
+#Where to go eat
+
+A simple JS app to help my partner decide where to go eat.
