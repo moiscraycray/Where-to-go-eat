74fd9f249feebef0a286c0c39871481133f5f356
Olivia Mo <oliviatrademark@gmail.com>
2018-05-01 13:38:45 +1000

    added logic to prevent the same place displaying twice in a row

--- index.js
+++ index.js
@@ -2,13 +2,28 @@
 
 var places = ['McDonalds', 'KFC', 'Hungry Jacks', 'Vietnamese', 'Sushi', 'Italian', 'Mexican', 'Turkish', 'Malaysian', 'Thai', 'Ramen', 'Burgers', 'Steak'];
 
+var firstRun = false;
+
 function randomiser() {
-  var decider = Math.floor(Math.random() * places.length);
-  console.log(decider);
-  return decider;
+  var number = Math.floor(Math.random() * places.length);
+  console.log(number);
+  return number;
+};
+
+function unique(method) {
+  var number = method();
+  if (firstRun === true) {
+    while (number === prevNum) {
+      number = method();
+      console.log(`number: ${number} ---- prevNum: ${prevNum}`);
+    }
+  }
+  var prevNum = number;
+  return number;
 };
 
 document.querySelector('.btn').addEventListener('click', () => {
-  var number = randomiser();
+  var number = unique(randomiser);
   document.querySelector('.go-here').innerHTML = places[number];
+  firstRun = true;
 });
